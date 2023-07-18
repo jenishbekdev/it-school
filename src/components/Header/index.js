feb4b75424df5d5328e6df3fd04a5a62f265922a
JenishbekDev <nurikovn87@gmail.com>
2023-07-18 16:59:20 +0600

    Master

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import  logo  from "../../img/headerlogo.svg"
+import './index.scss'
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          <div className="header__logo">
+            <img src={logo} alt="" />
+          </div>
+          <ul className="menu">
+            <li className="menu__item"><NavLink to={'/ourSchool'}>О школе</NavLink></li>
+            <li className="menu__item"><NavLink to={'/aboutcours'}>Наши курсы</NavLink></li>
+            <li className="menu__item"><NavLink to={'/aboutUs'}>О нас</NavLink></li>
+          </ul>
+          <div className="header__btn">
+            <Link>Войти</Link>
+            <button>Подписаться </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div id="homePage">
+      <div className="container">
+        <div className="homePage">
+          homePage
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
