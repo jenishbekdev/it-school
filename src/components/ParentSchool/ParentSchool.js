b371831dd2bdd3e1e7def0e9b7dbbf95c44f2ade
medinadevv <gulzina082@gmai.com>
2023-07-22 16:55:15 +0600

    hyu

--- src/components/ParentSchool/ParentSchool.js
+++ src/components/ParentSchool/ParentSchool.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import School from "./School/School";
 import Master from "./Master/Master";
-import Curs from "./Curs/index";
 import Card from "./Card";
 import SliderPage from "./SliderPage";
+
+
 
 const ParentSchool = () => {
     return (
