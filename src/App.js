fe51a5e6071f45df5edd2176b6b99f6131fba227
bektilek <sbektilek@gmail.com>
2023-07-22 12:10:47 +0600

    registr

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import Registr from './components/Registr';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Registr/>
       <div className='content'>
         <Routes>
           <Route path='/' element={<HomePage/>}/>
